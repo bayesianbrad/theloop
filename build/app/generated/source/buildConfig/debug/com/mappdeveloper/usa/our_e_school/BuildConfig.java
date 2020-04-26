703fd7fd3760ef1af5cf7be9bb83fbaab4149883
bayesianbrad <bradleygramhansen@gmail.com>
2020-04-26 21:04:40 +0100

    got app working with firebase. Still a lot to debug and change

---
+++ build/app/generated/source/buildConfig/debug/com/mappdeveloper/usa/our_e_school/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mappdeveloper.usa.our_e_school;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.theloop.elearn";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
